29814e6eb674907865eb2bc824e1346fe2811f88
vimalche <rskvimalraj@gmail.com>
2026-09-03 12:28:07 +0530

    Update trendpulse_task1.py

--- trendpulse_task1.py
+++ trendpulse_task1.py
@@ -5,7 +5,7 @@ import requests
 import json
 import time
 
-from datetime import datetime
+from datetime import datetime, timezone
 
 url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 headers = {"User-Agent": "TrendPulse/1.0"}
@@ -21,23 +21,46 @@ print("First 10 IDs:", story_ids[:10])
 
 ## get the details of each stories
 
+import os
+
+def get_category(title):
+  title_lower = title.lower()
+  if any(word in title_lower for word in ['ai','gpt''llm','model','machine learing']):
+    return "AI"
+  elif any(word in title_lower for word in ['python','code','software','web','app','github']):
+    return "TECH"
+  elif any(word in title_lower for word in['startup','business','market','money','pay']):
+    return "BUSINESS"
+  elif any(word in title_lower for word in['science','physics','space','biology','math','research']):
+    return "SCIENCE"
+  else:
+    return "OTHER"
+
 headers = {"User-Agent": "TrendPulse/1.0"}
 stories = []
-for id in story_ids:
+for id in story_ids[:125]:
   url = f"https://hacker-news.firebaseio.com/v0/item/{id}.json"
   response = requests.get(url, headers=headers)
   if response.status_code == 200:
     story = response.json()
-    stories.append(story) # Append the story to the list
+    if story and story.get("type") == 'story' and 'title' in story:
+      extracted_story = {"post_id" : story.get("id"),
+                         "title" : story.get("title"),
+                         "category" : story.get("title",""),
+                         "score" : story.get("score",0),
+                         "num_comments" : story.get("descendants",0),
+                         "author" : story.get("by"),
+                         "collected at" : datetime.now(timezone.utc).strftime("%Y-%m-%d %H-%M-%S")
+                         }
+    stories.append(story) 
+
   else:
     print(f"Error: {response.status_code}")
 
 print("Number of stories:", len(stories))
-print("First 10 stories:", stories[:10])
+print("First 10 stories:", stories[:1])
 
 ## create a json file
-
-import os
 
 os.makedirs("data", exist_ok=True)
 
